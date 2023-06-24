0cc63dfcb23c9929a24f091b9f4e377920eb75bd
Ricky Jiang <mr.jiangxue@hotmail.com>
2023-06-24 16:25:34 +0800

    invoke lambda using SDK

--- auction-frontend/app/page.tsx
+++ auction-frontend/app/page.tsx
@@ -1,3 +1,14 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+import AWS from 'aws-sdk';
+import { LambdaClient, InvokeCommand } from '@aws-sdk/client-lambda'
+
+
+export default async function Page() {
+  const client = new LambdaClient({})
+  const command = new InvokeCommand({
+    FunctionName: 'arn:aws:lambda:ap-south-1:399759979072:function:AuctionLambdaStack-SpacesLambdaA5F923A4-OV1KouOXlweL',
+    InvocationType: "RequestResponse",
+  });
+  const response = await client.send(command);
+  const result = new TextDecoder('utf-8').decode(response.Payload)
+  return <h1>123{JSON.stringify(result)}</h1>
 }
