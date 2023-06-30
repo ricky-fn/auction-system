925168e0c33f819d26f2b129354416b5e9e14b2e
Ricky Jiang <mr.jiangxue@hotmail.com>
2023-06-30 20:55:37 +0800

    save

--- auction-frontend/component/item/ListItem.tsx
+++ auction-frontend/component/item/ListItem.tsx
@@ -4,16 +4,16 @@ export default function ListItem() {
     <table className="w-full mb-2 text-md">
       <tbody>
         <tr>
-          <td className="px-6 py-4 text-left">
+          <td className="px-6 py-4 text-left w-1/4">
             <div className="font-medium text-gray-900">John Doe</div>
           </td>
-          <td className="px-6 py-4 text-center">
+          <td className="px-6 py-4 text-center w-1/4">
             <div className="text-gray-900">John Doe</div>
           </td>
-          <td className="px-6 py-4 text-center">
+          <td className="px-6 py-4 text-center w-1/4">
             <div className="text-gray-900">John Doe</div>
           </td>
-          <td className="px-6 py-4 text-right">
+          <td className="px-6 py-4 text-right w-1/4">
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
               Bid
             </button>
