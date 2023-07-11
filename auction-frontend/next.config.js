c7aa1516c238166756384208f09e3b921236bc65
Ricky Jiang <mr.jiangxue@hotmail.com>
2023-07-11 16:06:39 +0800

    test the next auth with different domain

--- auction-frontend/next.config.js
+++ auction-frontend/next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: false, // disable react strict mode to avoid rendering twice in dev mode https://stackoverflow.com/questions/71847778/why-my-nextjs-component-is-rendering-twice
   swcMinify: true,
   env: {
+    NEXTAUTH_URL: "https://main.d1tywrwo86r4jv.amplifyapp.com/",
     BASE_URL: process.env.BASE_URL,
     ENABLE_MOCKS: process.env.ENABLE_MOCKS,
   }
