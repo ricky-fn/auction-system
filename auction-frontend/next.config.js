b4918c923b02afced1e978a0274542e811d51cf2
Ricky Jiang <mr.jiangxue@hotmail.com>
2023-07-12 19:34:50 +0800

    save

--- auction-frontend/next.config.js
+++ auction-frontend/next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
   reactStrictMode: false, // disable react strict mode to avoid rendering twice in dev mode https://stackoverflow.com/questions/71847778/why-my-nextjs-component-is-rendering-twice
   swcMinify: true,
   env: {
-    NEXTAUTH_URL: "https://main.d1tywrwo86r4jv.amplifyapp.com/",
     BASE_URL: process.env.BASE_URL,
     ENABLE_MOCKS: process.env.ENABLE_MOCKS,
   }
