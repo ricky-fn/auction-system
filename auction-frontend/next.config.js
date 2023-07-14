81de5d3b00b36fd14a34b10c6ef3791e47d370f0
Ricky Jiang <mr.jiangxue@hotmail.com>
2023-07-14 20:41:42 +0800

    save

--- auction-frontend/next.config.js
+++ auction-frontend/next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 
+const dotenvExtended = require('dotenv-extended');
+
+if (process.env.NODE_ENV === 'test') {
+  dotenvExtended.load({
+    path: '.env.test.local',
+    defaults: '.env.test',
+    overrideProcessEnv: true
+  });
+}
+
 const nextConfig = {
   reactStrictMode: false, // disable react strict mode to avoid rendering twice in dev mode https://stackoverflow.com/questions/71847778/why-my-nextjs-component-is-rendering-twice
   swcMinify: true,
