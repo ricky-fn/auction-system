f16fcd86c34d789257a82c4a57e58d49af5da1a7
Ricky Jiang <mr.jiangxue@hotmail.com>
2023-07-14 14:46:26 +0800

    save

--- auction-frontend/cypress/support/commands.ts
+++ auction-frontend/cypress/support/commands.ts
@@ -4,6 +4,9 @@ import '@testing-library/cypress/add-commands'
 
 // Function logic derived from https://github.com/nextauthjs/next-auth/blob/5c1826a8d1f8d8c2d26959d12375704b0a693bfc/packages/next-auth/src/jwt/index.ts#L113-L121
 async function getDerivedEncryptionKey(secret: string) {
+  if (!secret) {
+    throw new Error("Encryption key is required to generate session token");
+  }
   return await hkdf(
     "sha256",
     secret,
