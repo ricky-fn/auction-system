c34e4cd6ef48d333fcc339ae1df614a4791465e2
Ricky Jiang <mr.jiangxue@hotmail.com>
2023-07-18 14:57:08 +0800

    should be read only

--- auction-frontend/app/item/[itemId]/page.tsx
+++ auction-frontend/app/item/[itemId]/page.tsx
@@ -78,7 +78,7 @@ export default async function Page({ params }: { params: { itemId: string } }) {
                       name="expirationTime"
                       type="text"
                       defaultValue={item?.expirationTime}
-                      placeholder="e.g., 1h"
+                      readOnly
                       className="border-0 ring-1 ring-inset ring-gray-300 block w-full rounded-md py-1.5 text-gray-900 shadow-sm sm:text-sm sm:leading-6"
                     />
                   </div>
